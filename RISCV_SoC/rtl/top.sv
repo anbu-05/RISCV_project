a513e126cbb2dd3a0499060bc7e0d1d5296c3e86
Anbu <anbuazhagudurai1@gmail.com>
2025-11-12 00:46:06 +0530

    created AXI mem and implemented read logic

--- RISCV_SoC/rtl/top.sv
+++ RISCV_SoC/rtl/top.sv
@@ -1,41 +1,41 @@
-module top #(
-) (
-    input logic clk,
-    input logic resetn
+module top (
+    input  logic clk,
+    input  logic resetn
 );
 
-	wire mem_valid;
-	wire mem_instr;
-	reg mem_ready;
-	wire [31:0] mem_addr;
-	wire [31:0] mem_wdata;
-	wire [3:0] mem_wstrb;
-	reg  [31:0] mem_rdata;
+    // Native PicoRV32 memory interface
+    wire        mem_valid;
+    wire        mem_instr;
+    wire [31:0] mem_addr;
+    wire [31:0] mem_wdata;
+    wire [3:0]  mem_wstrb;
+    wire [31:0] mem_rdata;
+    wire        mem_ready;
 
-    picorv32 #(
-    ) core (
-        .clk(clk),
-        .resetn(resetn),     
-        .mem_valid(mem_valid),
-        .mem_instr(mem_instr),
-        .mem_ready(mem_ready),
-        .mem_addr(mem_addr),
-        .mem_wdata(mem_wdata),
-        .mem_wstrb(mem_wstrb),
-        .mem_rdata(mem_rdata)
+    // PicoRV32 core
+    picorv32 core (
+        .clk        (clk),
+        .resetn     (resetn),
+        .mem_valid  (mem_valid),
+        .mem_instr  (mem_instr),
+        .mem_ready  (mem_ready),
+        .mem_addr   (mem_addr),
+        .mem_wdata  (mem_wdata),
+        .mem_wstrb  (mem_wstrb),
+        .mem_rdata  (mem_rdata)
     );
 
-    simple_mem #(
-    ) mem (
-        .clk(clk),
-        .resetn(resetn),     
-        .mem_valid(mem_valid),
-        .mem_instr(mem_instr),
-        .mem_ready(mem_ready),
-        .mem_addr(mem_addr),
-        .mem_wdata(mem_wdata),
-        .mem_wstrb(mem_wstrb),
-        .mem_rdata(mem_rdata)
+    // Simple memory model
+    simple_mem mem (
+        .clk        (clk),
+        .resetn     (resetn),
+        .mem_valid  (mem_valid),
+        .mem_instr  (mem_instr),
+        .mem_ready  (mem_ready),
+        .mem_addr   (mem_addr),
+        .mem_wdata  (mem_wdata),
+        .mem_wstrb  (mem_wstrb),
+        .mem_rdata  (mem_rdata)
     );
 
 endmodule
---
+++ RISCV_SoC/rtl/top_axi.sv
@@ -0,0 +1,103 @@
+module top_axi (
+    input  logic clk,
+    input  logic resetn
+);
+
+    // Native PicoRV32 memory interface
+    wire        mem_valid;
+    wire        mem_instr;
+    wire [31:0] mem_addr;
+    wire [31:0] mem_wdata;
+    wire [3:0]  mem_wstrb;
+    wire [31:0] mem_rdata;
+    wire        mem_ready;
+
+    // AXI4-Lite master interface signals
+    wire        mem_axi_awvalid;
+    wire        mem_axi_awready;
+    wire [31:0] mem_axi_awaddr;
+    wire [2:0]  mem_axi_awprot;
+
+    wire        mem_axi_wvalid;
+    wire        mem_axi_wready;
+    wire [31:0] mem_axi_wdata;
+    wire [3:0]  mem_axi_wstrb;
+
+    wire        mem_axi_bvalid;
+    wire        mem_axi_bready;
+
+    wire        mem_axi_arvalid;
+    wire        mem_axi_arready;
+    wire [31:0] mem_axi_araddr;
+    wire [2:0]  mem_axi_arprot;
+
+    wire        mem_axi_rvalid;
+    wire        mem_axi_rready;
+    wire [31:0] mem_axi_rdata;
+
+    // PicoRV32 core
+    picorv32 core (
+        .clk        (clk),
+        .resetn     (resetn),
+        .mem_valid  (mem_valid),
+        .mem_instr  (mem_instr),
+        .mem_ready  (mem_ready),
+        .mem_addr   (mem_addr),
+        .mem_wdata  (mem_wdata),
+        .mem_wstrb  (mem_wstrb),
+        .mem_rdata  (mem_rdata)
+    );
+
+    // Simple memory model
+    simple_mem mem (
+        .clk        (clk),
+        .resetn     (resetn),
+        .mem_valid  (mem_valid),
+        .mem_instr  (mem_instr),
+        .mem_ready  (mem_ready),
+        .mem_addr   (mem_addr),
+        .mem_wdata  (mem_wdata),
+        .mem_wstrb  (mem_wstrb),
+        .mem_rdata  (mem_rdata)
+    );
+
+    // PicoRV32 AXI adapter
+    picorv32_axi_adapter axi_adapter (
+        // Native interface
+        .clk        (clk),
+        .resetn     (resetn),
+        .mem_valid  (mem_valid),
+        .mem_instr  (mem_instr),
+        .mem_ready  (mem_ready),
+        .mem_addr   (mem_addr),
+        .mem_wdata  (mem_wdata),
+        .mem_wstrb  (mem_wstrb),
+        .mem_rdata  (mem_rdata),
+
+        // AXI interface
+            //Write Address Channel
+        .mem_axi_awvalid (mem_axi_awvalid),
+        .mem_axi_awready (mem_axi_awready),
+        .mem_axi_awaddr  (mem_axi_awaddr),
+        .mem_axi_awprot  (mem_axi_awprot),
+            //Write Data Channel
+        .mem_axi_wvalid  (mem_axi_wvalid),
+        .mem_axi_wready  (mem_axi_wready),
+        .mem_axi_wdata   (mem_axi_wdata),
+        .mem_axi_wstrb   (mem_axi_wstrb),
+            //Write Response Channel
+        .mem_axi_bvalid  (mem_axi_bvalid),
+        .mem_axi_bready  (mem_axi_bready),
+            //Read Address Channel
+        .mem_axi_arvalid (mem_axi_arvalid),
+        .mem_axi_arready (mem_axi_arready),
+        .mem_axi_araddr  (mem_axi_araddr),
+        .mem_axi_arprot  (mem_axi_arprot),
+            //Read Data Channel
+        .mem_axi_rvalid  (mem_axi_rvalid),
+        .mem_axi_rready  (mem_axi_rready),
+        .mem_axi_rdata   (mem_axi_rdata)
+            //a few mismatches ig? https://www.realdigital.org/doc/a9fee931f7a172423e1ba73f66ca4081
+    );
+
+endmodule
